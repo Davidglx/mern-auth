b572ce248585ee8e34e5ca2ca94112d32a0b085f
Davidglx <100171627+Davidglx@users.noreply.github.com>
2023-10-12 05:22:19 +0100

    completed profile page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import PrivateRoutes from "./components/PrivateRoutes";
